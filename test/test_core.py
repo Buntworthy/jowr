7e786fa124f69bccd03e040e8e691f2b3ed6371a
Buntworthy <justinpinkney@gmail.com>
2017-01-10 19:05:50 +0000

    Using fixtures

--- test/test_core.py
+++ test/test_core.py
@@ -15,20 +15,27 @@ import pytest
 # TODO use fixtures to provide test image and videos
 # TODO test window closing in show
 
-@patch('cv2.imshow')
-@patch('cv2.waitKey')
-def test_show(mock_waitKey, mock_imshow):
-    mock_waitKey.return_value = ord('k')
+@pytest.fixture
+def ones_image():
     width = 500
     height = 300
     test_image = np.zeros((height, width))
+    return {'width': width,
+            'height': height,
+            'image': test_image}
 
-    jowr.show(test_image)
+
+@patch('cv2.imshow')
+@patch('cv2.waitKey')
+def test_show(mock_waitKey, mock_imshow, ones_image):
+    mock_waitKey.return_value = ord('k')
+
+    jowr.show(ones_image['image'])
     mock_imshow.assert_called_once_with(jowr.core.DEFAULT_WINDOW_NAME,
-                                        test_image)
+                                        ones_image['image'])
     mock = Mock()
     mock.callback()
-    jowr.show(test_image, callbacks={'k': lambda: mock.callback})
+    jowr.show(ones_image['image'], callbacks={'k': lambda: mock.callback})
     mock.callback.assert_any_call()
 
 
@@ -43,12 +50,9 @@ def test_play(mock_waitKey, mock_imshow):
     mock_waitKey.call_count == len(video)
 
 
-def test_resolution():
-    width = 500
-    height = 300
-    test_image = np.zeros((height, width))
-
-    assert (jowr.resolution(test_image) == (width, height))
+def test_resolution(ones_image):
+    assert (jowr.resolution(ones_image['image']) == (ones_image['width'],
+                                                     ones_image['height']))
     with pytest.raises(TypeError):
         jowr.resolution('An image')
 
@@ -80,31 +84,24 @@ def test_find_images():
     assert set(found_images) == set(files)
 
 
-def test_scale():
-    width = 500
-    height = 300
+def test_scale(ones_image):
     scale = 0.5
-    test_image = np.zeros((height, width))
 
-    scaled_image = jowr.scale(test_image, scale)
+    scaled_image = jowr.scale(ones_image['image'], scale)
 
     im_shape = scaled_image.shape
-    assert im_shape[0] == scale * height
-    assert im_shape[1] == scale * width
+    assert im_shape[0] == scale * ones_image['height']
+    assert im_shape[1] == scale * ones_image['width']
 
 
-def test_add_to_zip():
-    width = 500
-    height = 300
-    test_image = np.ones((height, width))
-
+def test_add_to_zip(ones_image):
     with tempfile.TemporaryDirectory() as tmp_dir:
         tmp_file = os.path.join(tmp_dir, 'tmp.zip')
-        jowr.add_to_zip(test_image, tmp_file)
+        jowr.add_to_zip(ones_image['image'], tmp_file)
 
         with ZipFile(tmp_file) as tmp_zip:
             contents = tmp_zip.namelist()
             assert len(contents) == 1
             tmp_zip.extract(contents[0], path=tmp_dir)
             image = cv2.imread(os.path.join(tmp_dir, contents[0]))
-            assert np.sum(image[:, :, 0]) == width * height
+            assert np.sum(image[:, :, 0]) == ones_image['width'] * ones_image['height']
